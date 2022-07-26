05380858072139e22f7db440b763e5c941ce5ca9
Salim Reza <salim15-3117@diu.edu.bd>
2022-07-26 23:23:10 +0600

    .env save

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,65 +1,60 @@
 import { Text } from "react-native-paper";
-import React, { Component } from "react";
-import MapView, { Marker } from "react-native-maps";
+import React, { useEffect, useRef, useState } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View, Dimensions, Image } from "react-native";
 
-export class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: props.title,
-      contact: props.contact,
-      temp_cordinate: {
-        latitude: props.location.coordinates[1],
-        longitude: props.location.coordinates[0],
-      },
-    };
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          showsBuildings
-          ref={(ref) => {
-            this.map = ref;
-          }}
-          onRegionChange={() => {
-            this.map.animateCamera(
-              {
-                center: { latitute: this.state.temp_cordinate },
-                pitch: 45,
-                heading: 20,
-                altitude: 200,
-                zoom: 40,
-              },
-              1000
-            );
-          }}
-          region={{
-            latitude: this.state.temp_cordinate.latitude,
-            longitude: this.state.temp_cordinate.longitude,
+const Map = (props) => {
+  const _map = useRef(null);
+  const [temp_cordinate, setTemp_coords] = useState({
+    latitude: props?.location?.coordinates[1],
+    longitude: props?.location?.coordinates[0],
+  });
+
+  useEffect(() => {
+    if (_map.current) {
+      _map.current.animateCamera(
+        {
+          center: {
+            latitude: temp_cordinate.latitude,
+            longitude: temp_cordinate.longitude,
+          },
+          zoom: 15,
+        },
+        5000
+      );
+    }
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        showsTraffic
+        ref={_map}
+        initialRegion={{
+          latitude: temp_cordinate.latitude,
+          longitude: temp_cordinate.longitude,
+          latitudeDelta: 1 / 300,
+          longitudeDelta: 2 / 300,
+        }}
+        style={styles.mapStyle}
+      >
+        <Marker
+          coordinate={{
+            latitude: temp_cordinate.latitude,
+            longitude: temp_cordinate.longitude,
             latitudeDelta: 1 / 300,
             longitudeDelta: 2 / 300,
           }}
-          style={styles.mapStyle}
+          title={props.title || "সূর্যমুখি - ১"}
+          description={props.contact || "ড্রাইভার: ০১৬১৬৩৪৬৮৩৫"}
         >
-          <Marker
-            coordinate={{
-              latitude: this.state.temp_cordinate.latitude,
-              longitude: this.state.temp_cordinate.longitude,
-              latitudeDelta: 0.001,
-              longitudeDelta: 0.001,
-            }}
-            title={this.state.title || "সূর্যমুখি - ১"}
-            description={this.state.contact || "ড্রাইভার: ০১৬১৬৩৪৬৮৩৫"}
-          >
-            <Image source={require("../../assets/images/bus.png")} />
-          </Marker>
-        </MapView>
-      </View>
-    );
-  }
-}
+          <Image source={require("../../assets/images/bus.png")} />
+        </Marker>
+      </MapView>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
